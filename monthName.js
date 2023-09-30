f2ad43403949d46ee7764dcf5fe92577f35bf653
suren <tadevosyansuren07@gmail.com>
2023-09-30 12:28:34 +0400

    get month name by its number

---
+++ monthName.js
@@ -0,0 +1,20 @@
+function monthName(monthNum) {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  return months[monthNum - 1];
+}
+
+console.log(monthName(3));
